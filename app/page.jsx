058b2fd1e61ba903eb39ad012df583399af13be6
G0r4ni8 <kimga000408@gmail.com>
2023-12-09 22:10:04 +0900

    test

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,8 @@ export default function Home() {
     <>
       <div className="flex justify-between items-center">
         <div className="items-center">
-          <h1 className="text-3xl font-bold">Potato Market COMMUNITY</h1>
-          <p className="mb-4">Welcome to potato market community!</p>
+          <h1 className="text-3xl font-bold">Welcome To Goranie's Blog</h1>
+          <p className="mb4">"Show and Prove your self!"</p>
         </div>
       </div>
     </>
