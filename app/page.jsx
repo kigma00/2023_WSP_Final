bf642232f5d127bc7fd8e86080406452a78b58ce
G0r4ni8 <kimga000408@gmail.com>
2023-12-09 22:16:57 +0900

    test

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="flex justify-between items-center">
         <div className="items-center">
           <h1 className="text-3xl font-bold">Welcome To Goranie's Blog</h1>
-          <p className="mb4">"Show and Prove your self"</p>
+          <p className="mb4">"Show and Prove your self!"</p>
         </div>
       </div>
     </>
