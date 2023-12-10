7ba65ba3df48c1a3d7b0f040d5ffe8f32548e391
G0r4ni8 <kimga000408@gmail.com>
2023-12-11 00:57:57 +0900

    v2.6

--- models/log.js
+++ models/log.js
@@ -9,6 +9,6 @@ const logSchema = new Schema(
   }
 )
 
-const Log = mongoose.models.Log || mongoose.model('Log', logSchema)
+const User = mongoose.models.User || mongoose.model('Log', logSchema)
 
 export default Log
--- models/user.js
+++ models/user.js
@@ -9,6 +9,7 @@ const userSchema = new Schema(
     timestamps: true,
   }
 )
+
 const User = mongoose.models.User || mongoose.model('User', userSchema)
 
 export default User
